a13d3e6f858fb5b4353567e965906a70245303d8
virgilionkanu <virgiliocano53@gmail.com>
2023-09-26 22:26:20 -0300

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,10 +3,16 @@ import { CanceledError } from 'axios';
 import React, { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
